b306d26dfcd9d68664a12f3ee8bfc5cc0bf947dd
saianuragt <saianuragt@gmail.com>
2020-07-08 12:41:48 +0530

    FSD0620B105

--- src/Controller/MapServlet.java
+++ src/Controller/MapServlet.java
@@ -89,4 +89,3 @@ public class MapServlet extends HttpServlet {
 	}
 
 }
-
--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,35 @@
 package service;
+
+import java.util.*;
+
+import model.TouristPlace;
+
+public class ListOperations{
+	
+	static List<TouristPlace> list=new ArrayList<TouristPlace>();
+	
+	public List<TouristPlace> add(TouristPlace places){
+		list.add(places);
+		return list;
+	}
+	
+	public List<TouristPlace> remove(TouristPlace places){
+		list.remove(places);
+		return list;
+	}
+	
+	public Object sortByDestination(List<TouristPlace> places) {
+		Collections.sort(places,(m1,m2)->m1.getDestination().compareTo(m2.getDestination()));
+		return places;
+	}
+	
+	public Object sortByRank(List<TouristPlace> places) {
+		Collections.sort(places,(m1,m2)->m1.getRank().compareTo(m2.getRank()));
+		return places;
+	}
+	
+	public Object reset(List<TouristPlace> places) {
+		places.clear();
+		return places;
+	}
+}
